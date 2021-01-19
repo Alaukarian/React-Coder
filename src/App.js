3a1f75a74d49e461d69af1f4fdece8835f291909
Franco Parigi <hello@alaukarian.com>
2021-01-18 22:24:24 -0300

    Crea primer proyecto en React

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Modificacion para el proyecto. Link Activo
         </a>
       </header>
     </div>
